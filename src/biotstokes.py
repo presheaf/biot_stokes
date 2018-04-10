5a8a3061d0033abcf3d6e7962cd7236828a22265
Karl Erik Holter <karl0erik@gmail.com>
2018-04-10 18:39:03 +0200

    Added .geo file from Miro to  generate 3D domain

--- src/biotstokes.py
+++ src/biotstokes.py
@@ -1,20 +1,22 @@
-import xii
+# import xii
 import mshr
 import dolfin
 from block import block_mat, block_vec, block_bc
-from petsc4py import PETSc
+# from petsc4py import PETSc
 from dolfin import *
 from xii import *
 
-class BiotStokesDomain3D(object):
-    def __init__(self, N):
+
+class CylinderBoxDomain3D(object):
+    def __init__(self, hi, ho):
         EPS = 1E-3
         R = 0.25
-        box_domain = mshr.Box(dolfin.Point(0, 0, 0), dolfin.Point(1, 1, 1))
-        _mesh = mshr.generate_mesh(box_domain, N)
+        # box_domain = mshr.Box(dolfin.Point(0, 0, 0), dolfin.Point(1, 1, 1))
+        # _mesh = mshr.generate_mesh(box_domain, N)
+        _mesh = make_cylinder_box_mesh("mymesh.xml", hi, ho, R)
 
         stokes_subdomain = dolfin.CompiledSubDomain(
-            "sqrt((x[0]-0.5) * (x[0]-0.5) + (x[1]-0.5) * (x[1]-0.5)) < R", R=R
+            "sqrt(x[0] * x[0] + x[1] * x[1]) < R", R=R
         )
 
         subdomains = MeshFunction('size_t', _mesh, _mesh.topology().dim(), 0)
@@ -30,12 +32,15 @@ class BiotStokesDomain3D(object):
         submeshes, interface, _ = mortar_meshes(subdomains, range(2), strict=True, tol=EPS)
 
         self.full_domain = _mesh
-        self.stokes_domain = submeshes[0]
-        self.porous_domain = submeshes[1]
+        self.stokes_domain = submeshes[1]
+        self.porous_domain = submeshes[0]
         self.interface = interface
 
         self.mark_boundary()
-        
+
+    @property
+    def dimension(self):
+        return 3 
 
     def mark_boundary(self):
         """Interface should be marked as 0. Do not set BCs there.
@@ -60,7 +65,75 @@ class BiotStokesDomain3D(object):
 
         self.stokes_bdy_markers = stokes_markers
         self.porous_bdy_markers = porous_markers
+
+
+
+class AmbartsumyanMMSDomain(object):
+    def __init__(self, N):
+        EPS = 1E-3
+        R = 0.3
+        # rectangle_domain = mshr.Rectangle(dolfin.Point(0, -1), dolfin.Point(1, 1))
+        top_half = mshr.Rectangle(dolfin.Point(0, 0), dolfin.Point(1, 1))
+        bottom_half = mshr.Rectangle(dolfin.Point(0, -1), dolfin.Point(1, 0))
+        _topmesh = mshr.generate_mesh(top_half, N)
+        _botmesh = mshr.generate_mesh(bottom_half, N)
+
+        _mesh = mshr.DolfinMeshUtils.merge_meshes(_topmesh, _botmesh)
+        # _mesh = mshr.generate_mesh(rectangle_domain, N)
+        # _mesh = UnitSquareMesh(N, N)
+
+        stokes_subdomain = dolfin.CompiledSubDomain(
+            "x[1] > 0", R=R, eps=EPS
+        )
+
+        subdomains = MeshFunction('size_t', _mesh, _mesh.topology().dim(), 0)
+
+        # Awkward marking
+        for cell in cells(_mesh):
+            x = cell.midpoint().array()
+            if stokes_subdomain.inside(x, False):
+                subdomains[cell] = 1
+            else:
+                subdomains[cell] = 0
+
+        # strict = False here because in 2D, the interface is actually disconnected
+        submeshes, interface, _ = mortar_meshes(subdomains, range(2), strict=True, tol=EPS)
+
+        self.full_domain = _mesh
+        self.stokes_domain = submeshes[1]
+        self.porous_domain = submeshes[0]
+        self.interface = interface
+
+        self.mark_boundary()
+
+    @property
+    def dimension(self):
+        return 2
+
+    def mark_boundary(self):
+        """Interface should be marked as 0. Do not set BCs there.
+        left, right, top, bottom = 1, 2, 3, 4"""
         
+        stokes_markers = FacetFunction("size_t", self.stokes_domain, 0)
+        porous_markers = FacetFunction("size_t", self.porous_domain, 0)
+
+        interface_bdy = dolfin.CompiledSubDomain("on_boundary")
+
+        left_bdy = dolfin.CompiledSubDomain("near(x[0], 0) && on_boundary")
+        right_bdy = dolfin.CompiledSubDomain("near(x[0], 1) && on_boundary")
+        top_bdy = dolfin.CompiledSubDomain("near(x[1], 0) && on_boundary")
+        bottom_bdy = dolfin.CompiledSubDomain("near(x[1], 1) && on_boundary")
+
+        for markers in [stokes_markers, porous_markers]:
+            interface_bdy.mark(markers, 0)
+            left_bdy.mark(markers, 1)
+            right_bdy.mark(markers, 2)
+            top_bdy.mark(markers, 3)
+            bottom_bdy.mark(markers, 4)
+
+        self.stokes_bdy_markers = stokes_markers
+        self.porous_bdy_markers = porous_markers
+
 
 
 
@@ -101,20 +174,19 @@ class BiotStokesProblem(object):
 
         self.make_function_spaces()
 
-        self.add_neumann_bc("stokes", 1, 5)
-        self.add_neumann_bc("stokes", 2, 10)
+
         
     def add_dirichlet_bc(self, problem_name, subdomain_id, value):
         bc_dict = self._dirichlet_bcs[problem_name]
-        if subdomain_id in bc_dict:
-            del bc_dict[subdomain_id]
+        # if subdomain_id in bc_dict:
+        #     del bc_dict[subdomain_id]
             
         bc_dict[subdomain_id] = value
 
     def add_neumann_bc(self, problem_name, subdomain_id, value):
         bc_dict = self._neumann_bcs[problem_name]
-        if subdomain_id in bc_dict:
-            del bc_dict[subdomain_id]
+        # if subdomain_id in bc_dict:
+        #     del bc_dict[subdomain_id]
             
         bc_dict[subdomain_id] = value
         
@@ -132,6 +204,8 @@ class BiotStokesProblem(object):
         X = FunctionSpace(self.domain.interface, "DG", 1)
 
         self.W = [Vp, Qp, U, Vf, Qf, X]
+        print("dofs: {}".format(sum([sp.dim() for sp in self.W])))
+
 
 
     def get_solver(self):
@@ -176,9 +250,9 @@ class BiotStokesProblem(object):
         Tvp, Tep, Tvf = map(lambda x: Trace(x, self.domain.interface),
                             [vp, ep, vf]
         )
-        Tr = lambda x: Trace(x, self.domain.interface)
 
-        n_Gamma_f = OuterNormal(self.domain.interface, [0.5, 0.5, 0.5]) # todo: verify that this is actually the right normal
+        # todo: verify that this is actually the right normal
+        n_Gamma_f = OuterNormal(self.domain.interface, [0.5] * self.domain.dimension)
         n_Gamma_p = -n_Gamma_f
         
         # build a bunch of matrices
@@ -225,41 +299,47 @@ class BiotStokesProblem(object):
             [npvpt, 0, Constant(1/dt) * npept , nfvft, 0, 0],
         ]
 
-        # RHS
-        # TODO: put Neumann BCs here
 
-        nf = FacetNormal(self.domain.stokes_domain)
-        np = FacetNormal(self.domain.porous_domain)
-
-        L_pp = inner(vp, np) * Constant(0) * dsDarcy 
-        L_interface = Constant(Cp) * inner(vp, n_Gamma_p) * dxGamma
-        bpp = (
-            Constant(s0 / dt) * pp_prev * wp * dxDarcy
-            - Constant(alpha / dt) * inner(div(dp_prev), wp) * dxDarcy
-        )
-        L_darcy = Constant(1 / dt) * Constant(0) * inner(np, ep) * dsDarcy
-        L_stokes = Constant(0) * inner(nf , vf) * dsStokes
-
-        # L_mult = Constant(1 / dt) * inner(dp_prev, n_Gamma_p) * mu * dxGamma   # TODO: figure out why this doesn't work
-        L_mult = Constant(0) * mu * dxGamma
-        
-        L = [
-            L_pp + L_interface,
-            -bpp,
-            L_darcy,
-            L_stokes,
-            Constant(0) * wf * dsStokes,
-            L_mult
-
-        ]
 
         # quick sanity check
         N_unknowns = 6
         assert len(a) == N_unknowns
         for row in a:
             assert len(row) == N_unknowns
-        assert len(L) == N_unknowns
-        
+
+        def compute_RHS(dp_prev, neumann_bcs):
+            # TODO: put Neumann BCs here
+
+            nf = FacetNormal(self.domain.stokes_domain)
+            np = FacetNormal(self.domain.porous_domain)
+
+            L_pp = inner(vp, np) * Constant(0) * dsDarcy 
+            L_interface = Constant(Cp) * inner(Tvp, n_Gamma_p) * dxGamma
+            bpp = (
+                Constant(s0 / dt) * pp_prev * wp * dxDarcy
+                - Constant(alpha / dt) * inner(div(dp_prev), wp) * dxDarcy
+            )
+            L_darcy = Constant(1 / dt) * Constant(0) * inner(np, ep) * dsDarcy
+            L_stokes = Constant(0) * inner(nf , vf) * dsStokes
+
+            Tdp_prev = Trace(dp_prev, self.domain.interface)
+            L_mult = Constant(1 / dt) * inner(Tdp_prev, n_Gamma_p) * mu * dxGamma   
+
+            L = [
+                L_pp + L_interface,
+                -bpp,
+                L_darcy,
+                L_stokes,
+                Constant(0) * wf * dsStokes,
+                L_mult
+
+            ]
+
+            assert len(L) == N_unknowns
+            
+            return L
+
+            
         # bcs 
         up_bcs = [
             DirichletBC(
@@ -314,51 +394,90 @@ class BiotStokesProblem(object):
             t += dt
             # todo: set t for BCs if time-dependent and if so, reapply to A
 
-            
+            L = compute_RHS(dp_prev, self._neumann_bcs)
             bb = ii_assemble(L)
             bbcs.apply(bb)
             bbm = ii_convert(bb)            
             
-            # bb = None
-            # bbcs.apply(bb)
             solver.solve(w.vector(), bbm)
 
             for i, func in enumerate([up_prev, pp_prev, dp_prev, uf_prev, pf_prev, lbd_prev]):
                 func.assign(w[i])
 
             yield w
-        
-        
+
+
+def doit():
+    # N = 10
+    hi, ho = 0.1, 0.1
+    # domain = AmbartsumyanMMSDomain(N)
+    domain = CylinderBoxDomain3D(hi, ho)
+
+    problem = BiotStokesProblem(domain, {})
+    solution = problem.get_solver()
+    problem.add_neumann_bc("stokes", 1, 5)
+    problem.add_neumann_bc("stokes", 2, 10)
+
+    
+    FunctionSpace
+    print("First call to nesxt()")
+    funcs = solution.next()
+    print("Done with first call to next() !!")
+    # solution.next()
+    # funcs = solution.next()
+    # print("2 and 3 done!!")
+    
+    def save_to_file(things, fns=None):
+        if fns is None:
+            fns = ["tmp{}.pvd".format(i) for i, _ in enumerate(things)]
+        for thing, fn in zip(things, fns):
+            f = dolfin.File(fn)
+            f << thing
+
+    save_to_file(
+        funcs,
+        ["up.pvd", "pp.pvd", "dp.pvd", "uf.pvd", "pf.pvd", "lbd.pvd"],
+    )
+    save_to_file(
+        [domain.stokes_domain, domain.porous_domain,
+         domain.interface, domain.full_domain],
+        ["stokes.pvd", "biot.pvd", "interface.pvd", "full.pvd"],
+    )
+
+
+
+
+def make_cylinder_box_mesh(mesh_fn, hi, ho, R):
+    import subprocess, os
+    try:
+        os.remove(mesh_fn)
+    except:
+        pass
+    
+    with open("cylinderboxtemplate.geo", "r") as f:
+        text = "".join(f.readlines())
+
+    
+    text = text.replace("__HI__", str(hi), 1)
+    text = text.replace("__HO__", str(ho), 1)
+    text = text.replace("__R__", str(R), 1)
+    
+    tmp_geo_fn = "temp_geo.geo"
+    tmp_msh_fn = "temp_geo.msh"
+    with open(tmp_geo_fn, "w") as f:
+        f.write(text)
+
+    subprocess.call(["gmsh", "-3", tmp_geo_fn])
+    subprocess.call(["dolfin-convert", tmp_msh_fn, mesh_fn])
+
+    os.remove(tmp_geo_fn)
+    os.remove(tmp_msh_fn)
+
+    return Mesh(mesh_fn)
+
+
+    
+    
     
 
-
-N = 10
-# domain = BiotStokesDomain2D(N)
-domain = BiotStokesDomain3D(N)
-save_to_file(
-    [domain.stokes_domain, domain.porous_domain,
-     domain.interface, domain.full_domain],
-    ["stokes.pvd", "biot.pvd", "interface.pvd", "full.pvd"],
-)
-
-problem = BiotStokesProblem(domain, {})
-solution = problem.get_solver()
-
-print("First call to nesxt()")
-solution.next()
-print("Done with first call to nesxt() !!")
-solution.next()
-print("Done with second call to nesxt() !!")
-solution.next()
-solution.next()
-print("3 and 4 done!!")
-
-def save_to_file(things, fns=None):
-    if fns is None:
-        fns = ["tmp{}.pvd".format(i) for i, _ in enumerate(things)]
-    for thing, fn in zip(things, fns):
-        f = dolfin.File(fn)
-        f << thing
-
-
-# doit()
+doit()
